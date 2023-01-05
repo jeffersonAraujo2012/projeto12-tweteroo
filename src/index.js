80a3432ca065564ba48e1794994b740cf0f50a21
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-01-05 09:23:14 -0300

    Rota sign-up feita.

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const users = [
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+];
+const tweets = [
+  {
+    username: "bobesponja",
+    avatar:
+      "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+    tweet: "eu amo o hub",
+  },
+];
+
+app.post("/sign-up", (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.send("OK");
+});
+
+app.listen(5000, () => {
+  console.log("servidor online");
+});
