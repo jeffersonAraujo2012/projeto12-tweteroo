a225fcd0813c02ff4bae99a1da7405e6ba166c68
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-01-05 10:55:42 -0300

    Arrays globais 'users' e 'tweets' tornados inicialmente vazios.

--- src/index.js
+++ src/index.js
@@ -5,20 +5,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const users = [
-  {
-    username: "bobesponja",
-    tweet: "eu amo o hub",
-  },
-];
-const tweets = [
-  {
-    username: "bobesponja",
-    avatar:
-      "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-    tweet: "eu amo o hub",
-  },
-];
+const users = [];
+const tweets = [];
 
 app.post("/sign-up", (req, res) => {
   const user = req.body;
